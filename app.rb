70748d705430228f60dd79cd7b8bf86696070545
assane savadogo <assane.savadogo81@gmail.com>
2019-10-10 18:02:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'pry'
 class Application < Sinatra::Base
+<<<<<<< HEAD
 
   get '/' do 
     erb :index
   end
 
+=======
+  get '/' do 
+    erb :index
+  end
+  
+>>>>>>> 09f13ca53812883ea9fefc4ec6b430811f184eb1
   post '/greet' do 
     erb :greet 
   end
--- config.ru
+++ config.ru
@@ -1,4 +1,10 @@
 require 'sinatra'
+<<<<<<< HEAD
 require_relative './app'
 
 run Application
+=======
+require_relative './app.rb'
+
+run Application
+>>>>>>> 09f13ca53812883ea9fefc4ec6b430811f184eb1
